70de6f93d743ed308bcbaa42f982a02a37225ad5
dahne duffy <12duffd@gmail.com>
2022-06-14 22:26:13 -0600

    undirected graph hasPath traversal

--- dataStructures.js
+++ dataStructures.js
@@ -185,7 +185,7 @@ newPriorityQueue.enqueue(['c',1])
 //Binary tree only has max two branches for each node, and they are ordered; the left subtree of a node contains only nodes with keys lesser than the node's key; the right subtree of a node contains only nodes with keys greater than the node's key; the left and right subtree must alsobe a binary search tree; it take o(log(n)) time
 
 class Node{
-    constructor(date, left=null,right=null){
+    constructor(data, left=null,right=null){
         this.data = data;
         this.left = left;
         this.right = right;
@@ -228,10 +228,293 @@ class BinarySearchTree{
         }
     }
     findMin(){
-
+        let current = this.root;
+        while(current.left !== null){
+            current = current.left
+        }
+        return current.data;
     }
+    findMax(){
+        let current = this.root;
+        while(current.right !== null){
+            current = current.right
+        }
+        return current.data;
+    }
+    find(data){
+        let current = this.root;
+        while(current.data !== data){
+            if (data < current.data){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+            if(current === null){
+                return null
+            }
+        }
+        return current;
+    }
+    isPresent(data){
+        let current = this.root;
+        //while current is not null
+        while(current){
+            if(data === current.data){
+                return true;
+            }
+            if (data < current.data){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+    remove(data) {
+        const removeNode = function(node, data) {
+            console.log(node,data)
+          if (node == null) {
+            return null;
+          }
+          if (data == node.data) {
+            // node has no children 
+            if (node.left == null && node.right == null) {
+              return null;
+            }
+            // node has no left child 
+            if (node.left == null) {
+              return node.right;
+            }
+            // node has no right child 
+            if (node.right == null) {
+              return node.left;
+            }
+            // node has two children 
+            //from node, move right, then find the left most node (i.e. find the minimum of the right subtree)
+            var tempNode = node.right;
+            while (tempNode.left !== null) {
+              tempNode = tempNode.left;
+            }
+            //set the current node, to have the value of the left most node you just found
+            node.data = tempNode.data;
+            //then run the function again from the 
+            node.right = removeNode(node.right, tempNode.data);
+            return node;
+
+        //if you're not at the right node, move left or right
+          } else if (data < node.data) {
+            node.left = removeNode(node.left, data);
+            return node;
+          } else {
+            node.right = removeNode(node.right, data);
+            return node;
+          }
+        }
+        this.root = removeNode(this.root, data);
+      }
+      isBalanced(){
+          return (this.findMinHeight() >= this.findMaxHeight()-1)
+          //will return true or false
+      }
+      //this is the distance from the root node to the first leaf node without two children
+      findMinHeight(node = this.root){
+          //default input is root node
+          if(node == null){
+              return -1;
+          };
+          //if nothing is in the tree, height is -1
+          let left = this.findMinHeight(node.left);
+          let right = this.findMin(node.right);
+          if(left < right){
+              return left + 1;
+          } else {
+              return right + 1;
+          }
+      }
+      //distance from the root node to the most bottom node
+      //NOTE A BALANCED TREE IS WHERE MIN AND MAX DIFFER BY AT MOST ONE
+      findMaxHeight(node = this.root){
+        //default input is root node
+        if(node == null){
+            return -1;
+        };
+        //if nothing is in the tree, height is -1
+        let left = this.findMinHeight(node.left);
+        let right = this.findMin(node.right);
+        if(left > right){
+            return left + 1;
+        } else {
+            return right + 1;
+        }
+    }
+    //TREE TRAVERSAL METHODS
+    //inOrder() traversal --> begin search at left most node, and at rightmost; effectively in order from lowest to highest
+    //preOrder() traversal --> explore roots nodes before leaves; start at root and go all the way left, then right
+    //postOrder() traversal --> explores the leaf nodes first, essentially sstart at left-most and lowest leaf nodes and work right
+    //levelOrder() traversal --> explores each level of the tree
+    inOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traverseInOrder(node) { 
+              //recursive function - if there is a left node, run this function again, until there is no left node, then push the node.data of the left-most node onto your array, and look right.  If there is node,right run this function again. Essentially you are looking left until you can't anymore and then storing that data, you then work your way back up the call stack (the other nodes), until everything has been stored in the resultant array in numerical order
+            node.left && traverseInOrder(node.left);
+            result.push(node.data);
+            node.right && traverseInOrder(node.right);
+          }
+          traverseInOrder(this.root);
+          return result;
+        };
+      }
+      preOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traversePreOrder(node) {
+            result.push(node.data);
+            node.left && traversePreOrder(node.left);
+            node.right && traversePreOrder(node.right);
+          };
+          traversePreOrder(this.root);
+          return result;
+        };
+      }
+      postOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traversePostOrder(node) {
+            node.left && traversePostOrder(node.left);
+            node.right && traversePostOrder(node.right);
+            result.push(node.data);
+          };
+          traversePostOrder(this.root);
+          return result;
+        }
+      }
+      
+      levelOrder() {
+          let result = [];
+          let Q = []; 
+          if (this.root != null) {
+              Q.push(this.root);
+              while(Q.length > 0) {
+                  let node = Q.shift();
+                  result.push(node.data);
+                  if (node.left != null) {
+                      Q.push(node.left);
+                  };
+                  if (node.right != null) {
+                      Q.push(node.right);
+                  };
+              };
+              return result;
+          } else {
+              return null;
+          };
+      };
+
 }
 
-//left off at 31:59 
+const bst = new BinarySearchTree();
+
+bst.add(9);
+bst.add(4);
+bst.add(17);
+bst.add(3);
+bst.add(6);
+bst.add(22);
+bst.add(5);
+bst.add(7);
+bst.add(20);
+bst.add(10);
+
+console.log(bst.levelOrder())
+//run this in the Chrome debugger
 
 //https://www.youtube.com/watch?v=t2CEgPsws3U
+
+//HASH TABLES are used to store key value pairs, associative arrays, a common way to implement mapped data structure.  Highly used because of efficient, average time complexity is O(1) for search insert and delete;
+//It takes a key input and runs it through a hash function; a hash function basically maps strings to numbers, where numbers correspond to indexes in an array. The array index corresponds to a bucket, which stores some information.  
+//You'll probably never have to implement a hash table, but this might be useful:
+
+/* Hash Table */
+
+var hash = (string, max) => {
+    var hash = 0;
+    for (var i = 0; i < string.length; i++) {
+      hash += string.charCodeAt(i);
+    }
+    return hash % max;
+  };
+  
+  let HashTable = function() {
+  
+    let storage = [];
+    const storageLimit = 14;
+    
+    this.print = function() {
+      console.log(storage)
+    }
+  
+    this.add = function(key, value) {
+      var index = hash(key, storageLimit);
+      if (storage[index] === undefined) {
+        storage[index] = [
+          [key, value]
+        ];
+      } else {
+        var inserted = false;
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            storage[index][i][1] = value;
+            inserted = true;
+          }
+        }
+        if (inserted === false) {
+          storage[index].push([key, value]);
+        }
+      }
+    };
+  
+    this.remove = function(key) {
+      var index = hash(key, storageLimit);
+      if (storage[index].length === 1 && storage[index][0][0] === key) {
+        delete storage[index];
+      } else {
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            delete storage[index][i];
+          }
+        }
+      }
+    };
+  
+    this.lookup = function(key) {
+      var index = hash(key, storageLimit);
+      if (storage[index] === undefined) {
+        return undefined;
+      } else {
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            return storage[index][i][1];
+          }
+        }
+      }
+    };
+  
+  };
+  
+  
+  console.log(hash('quincy', 10))
+  
+  let ht = new HashTable();
+  ht.add('beau', 'person');
+  ht.add('fido', 'dog');
+  ht.add('rex', 'dinosour');
+  ht.add('tux', 'penguin')
+  console.log(ht.lookup('tux'))
+  ht.print();
--- graphs.js
+++ graphs.js
@@ -1,98 +1,185 @@
-//GRAPHS
+// //GRAPHS
 
-//A graph is a collection of nodes(vertices) and edges, i.e. nodes and the connections between them
+// //A graph is a collection of nodes(vertices) and edges, i.e. nodes and the connections between them
 
-//You can model maps (e.g. road between cities);
+// //You can model maps (e.g. road between cities);
 
-//TERMINOLOGY   
-//directed graph - there is directionality from node to node
-//undirected graph - there is no directionality from node to node
-//neighbor nodes - directly adjacent nodes
-//acyclic graphs - no cycles on graphs whereby you can start at one node at end up back there
+// //TERMINOLOGY   
+// //directed graph - there is directionality from node to node
+// //undirected graph - there is no directionality from node to node
+// //neighbor nodes - directly adjacent nodes
+// //acyclic graphs - no cycles on graphs whereby you can start at one node at end up back there
 
-//IMPLEMENTATION
-//Typically, "adjacency lists" can represent graphs, which have constant lookup times
-//The keys of the adjacency list will be every node in the graph, and their corresponding values will be an array of the neighbors of that particular node
- //e.g. look below for an example representing a directed graph
+// //IMPLEMENTATION
+// //Typically, "adjacency lists" can represent graphs, which have constant lookup times
+// //The keys of the adjacency list will be every node in the graph, and their corresponding values will be an array of the neighbors of that particular node
+//  //e.g. look below for an example representing a directed graph
 
- const graph = {
-     a: ['b','c'],
-     b:['d'],
-     c:['e'],
-     d:['f'],
-     e:[],
-     f:[]
- }
+//  const graph = {
+//      a: ['b','c'],
+//      b:['d'],
+//      c:['e'],
+//      d:['f'],
+//      e:[],
+//      f:[]
+//  }
 
- //ALGORITHMS
- //***************************************************************************** */
+//  //ALGORITHMS
+//  //***************************************************************************** */
 
- //DEPTH FIRST TRAVERSAL
- //travel in a specific direction until you can't anymore, then go in a different direction - uses a stack
- //PSEUDO - move in a direction and add each node to a stack until you reach a terminal node.  When you pop a node off of the stack, it becomes your current node, form which you continue exploring. 
-//IMPLEMENTATION
-const depthFirstPrintIterative = (graph,source)=>{
-    const stack = [source];
+//  //DEPTH FIRST TRAVERSAL
+//  //travel in a specific direction until you can't anymore, then go in a different direction - uses a stack
+//  //PSEUDO - move in a direction and add each node to a stack until you reach a terminal node.  When you pop a node off of the stack, it becomes your current node, form which you continue exploring. 
+// //IMPLEMENTATION
+// const depthFirstPrintIterative = (graph,source)=>{
+//     const stack = [source];
 
-    while (stack.length>0){
-        const current = stack.pop()
-        console.log(current)
-        for(let neighbor of graph[current]){
-            stack.push(neighbor);
-        }
-    }
-};
-//EXPLANATION - initialize the stack, with the source node as the only value; while the stack is not empty, pop the current node off and log it; then explore each neighbor of that node and push them onto the stack.  then repeat.  
+//     while (stack.length>0){
+//         const current = stack.pop()
+//         console.log(current)
+//         for(let neighbor of graph[current]){
+//             stack.push(neighbor);
+//         }
+//     }
+// };
+// //EXPLANATION - initialize the stack, with the source node as the only value; while the stack is not empty, pop the current node off and log it; then explore each neighbor of that node and push them onto the stack.  then repeat.  
 
-const depthFirstPrintRecursive = (graph,source)=>{
-    console.log(source);
+// const depthFirstPrintRecursive = (graph,source)=>{
+//     console.log(source);
     
-    for(let neighbor of graph[source]){
-        depthFirstPrintRecursive(graph,neighbor)
-    }
-};
-//EXPLANATION - print the current node, then run a loop that recursively calls the function for each neighbor node. 
+//     for(let neighbor of graph[source]){
+//         depthFirstPrintRecursive(graph,neighbor)
+//     }
+// };
+// //EXPLANATION - print the current node, then run a loop that recursively calls the function for each neighbor node. 
 
-depthFirstPrintIterative(graph,'a') //acedbdf
-depthFirstPrintRecursive(graph,'a') //abdfce 
+// // depthFirstPrintIterative(graph,'a') //acedbdf
+// // depthFirstPrintRecursive(graph,'a') //abdfce 
 
-//******************************************************************* */
- //BREADTH FIRST TRAVERSAL
- //explore all immediate neighbors of each node, and then keep applying that behavior - uses a queue
- //PSEUDO - start with the current node, and then push the neighbor nodes into a queue; this ends the first iteration.  Then you grab the node from the front of queue and then continue iterating/searching.  
-
-
- //IMPLEMENTATION
- const breadthFirstPrint = (graph,source)=>{
-     const queue = [source];
-
-     while(queue.length>0){
-        const current = queue.shift();
-        console.log(current);
-
-        for(let neighbor of graph[current]){
-            queue.push(neighbor)
-        }
-     }
- }
-
-breadthFirstPrint(graph,'a') //abcdef
-
-//EXPLANATION - initialize the queue, with the source node as the only value; while the queue is not empty, shift the current node off the front and log it; then explore each neighbor of that node and push them onto the queue.  then repeat.
-//Note - recursion for breadth is tricky, because the call stack has a stack structure, so you have to work to overcome that;  
+// //******************************************************************* */
+//  //BREADTH FIRST TRAVERSAL
+//  //explore all immediate neighbors of each node, and then keep applying that behavior - uses a queue
+//  //PSEUDO - start with the current node, and then push the neighbor nodes into a queue; this ends the first iteration.  Then you grab the node from the front of queue and then continue iterating/searching.  
 
 
+//  //IMPLEMENTATION
+//  const breadthFirstPrint = (graph,source)=>{
+//      const queue = [source];
+
+//      while(queue.length>0){
+//         const current = queue.shift();
+//         console.log(current);
+
+//         for(let neighbor of graph[current]){
+//             queue.push(neighbor)
+//         }
+//      }
+//  }
+
+// // breadthFirstPrint(graph,'a') //abcdef
+
+// //EXPLANATION - initialize the queue, with the source node as the only value; while the queue is not empty, shift the current node off the front and log it; then explore each neighbor of that node and push them onto the queue.  then repeat.
+// //Note - recursion for breadth is tricky, because the call stack has a stack structure, so you have to work to overcome that;  
 
 
-//HAS PATH PROBLEM 1 - directed graph - return true or false regarding whether there is path between two given nodes.
 
-let adjacencyList = {
-    f:[g,i],
-    g:[h],
-    h:[],
-    i:[g,k],
-    j:[i],
-    k:
-}
+
+// //HAS PATH PROBLEM 1 - directed graph - return true or false regarding whether there is path between two given nodes.
+// //Can you travel from source node to desination node?
+// //you'll have O(n) time complexity
+
+// let adjacencyList = {
+//     f:['g','i'],
+//     g:['h'],
+//     h:[],
+//     i:['g','k'],
+//     j:['i'],
+//     k:[]
+// }
+
+// const hasPathDepth = (graph, src, dst) => {
+//     if (src === dst) return true;
+
+//     for(let neighbor of graph[src]){
+//         if(hasPath(graph,neighbor,dst)){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// const hasPathBreadth = (graph, src, dst) => {
+//     const queue = [src];
+
+//     while(queue.length>0){
+//         const current = queue.shift();
+//         if(current === dst){
+//             return true
+//         }
+//         for (let neighbor of graph[current]){
+//             queue.push(neighbor);
+//         }
+//     }
+//     return false;
+// }
+
+// const result = hasPathBreadth(adjacencyList,'h','k')
+// console.log(result)
+
+
 
 // video link:  https://www.youtube.com/watch?v=tWVWeAqZ0WU at 30:09
+
+//UNDIRECTED GRAPH
+
+let edges = [['i','j'],['k','i'],['m','k'],['k','l'],['o','n']]
+//convert to adjacency list: 
+
+let edgesGraph = {
+    i: ['j','k'],
+    j: ['i'],
+    k: ['i','m','l'],
+    m: ['k'],
+    l: ['k'],
+    o: ['n'],
+    n: ['o']
+}
+
+//do a traversal, and mark nodes as 'visited'
+const undirectedPathDepth = (edges, nodeA, nodeB)=>{
+    const graph = buildGraph(edge);
+
+    return hasPath(graph,nodeA,nodeB, new Set());
+}
+
+const hasPath = (graph,src,dst,visited)=>{
+    //check if node has been visited or if you're already at destination node
+    if(visited.has(src)) return false;
+    if(src === dst) return true;
+
+    //if not visited, add node to 'visited set'
+    visited.add(src);
+
+    //recursive loop through the nodes until destination node is found;
+    for(let neighbor of graph[src]){
+        if(hasPath(graph,neighbor,dst,visited)===true){
+            return true
+        }
+    }
+    return false;
+}
+
+//essentially grab each set of edges and push them into an adjacency list object;
+const buildGraph = (edges)=>{
+    const graph = {};
+    for(let edge of edges){
+        const [a,b] = edge
+        if (!(a in graph)) graph[a]=[];
+        if (!(b in graph)) graph[b]=[];
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+}
+
+//left off at 1:00:13
